7b1d63f67667a464acce6075d7322fa8fccef5d3
glassive <phileas.sibien@gmail.com>
2025-11-20 12:23:54 +0100

    remove unused files

--- js/bg.js
+++
@@ -1,5 +0,0 @@
-console.log("eoiferiojg")
-penta = document.getElementById("pentagon");
-penta.style.position = "absolute";
-d.style.left = Math.random(20,80)+'%';
-d.style.top = Math.random(20,80)+'%';
